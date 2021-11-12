186925e895baa134f5fc25d9dab6650b26652016
PaulaMH <paulamorenohermoso99@gmail.com>
2021-11-12 04:45:56 +0100

    "Actualizado ej6 fase 4 JavaScript"

--- Paula Moreno Hermoso, 2ºDAW/UD. 3/Fase_4/Ej. 6/ej6.js
+++ Paula Moreno Hermoso, 2ºDAW/UD. 3/Fase_4/Ej. 6/ej6.js
@@ -1,25 +1,10 @@
 //Creamos una funcion que recibe como parametro una matriz
 function maximo (matriz)
-{
-    //Definimos una variable max y la igualamos al 
-    //mínimo valor numérico posible
-    let max = Number.MIN_VALUE;
-
-    //Creamos un bucle que recorre la matriz
-    for (let i = 0; i < matriz.length; i++)
-    {
-        //Creamos condicional para ver si el valor
-        //del array en la posicion i es mayor que
-        //la variable maximo
-        if(max < matriz[i])
-        {
-            max = matriz[i];
-        }
-    }
-
-    //Devolvemos el maximo
-    return max;
+{   
+    //Utilizamos la funcion de la clase Math max junto
+    //con el metodo apply para sacar el valor más
+    //alto de la matriz
+    return Math.max.apply(null, matriz);
 }
-
 let matriz = [1, 6, 3, 4, 5, 0];
 alert("Valor más alto de la matriz: " + maximo(matriz));
