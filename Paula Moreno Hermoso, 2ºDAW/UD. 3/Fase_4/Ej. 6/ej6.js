07dc9a2a9600630bd919591133ca1aff28d13cca
PaulaMH <paulamorenohermoso99@gmail.com>
2021-11-12 04:50:49 +0100

    "Actualizado ej 6 fase 4 JavaScript"

--- Paula Moreno Hermoso, 2ºDAW/UD. 3/Fase_4/Ej. 6/ej6.js
+++ Paula Moreno Hermoso, 2ºDAW/UD. 3/Fase_4/Ej. 6/ej6.js
@@ -6,5 +6,5 @@ function maximo (matriz)
     //alto de la matriz
     return Math.max.apply(null, matriz);
 }
-let matriz = [1, 6, 3, 4, 5, 0];
+let matriz = [11,3,16,90];
 alert("Valor más alto de la matriz: " + maximo(matriz));
